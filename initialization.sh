b5dcefef2785a84909716e8023b380b61b2b8a78
josef94 <josef.boeckle@gmail.com>
2017-06-22 08:39:50 +0200

    initialization.sh updated

--- initialization.sh
+++ initialization.sh
@@ -13,45 +13,163 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
-echo -e  "$Cyan Update $OFF"
+############################# Update & Upgrade #############################
+echo -e  "$Cyan Update... $OFF"
 sudo apt-get -y -q update
-echo -e "$Cyan Upgrade $OFF"
+echo -e "$Cyan Upgrade... $OFF"
 sudo apt-get -y -q upgrade
 
-echo -e "$Cyan Install OpenCV $OFF"
-sudo apt-get -y -q install build-essential
-sudo apt-get -y -q install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-cd ../../
-git clone https://github.com/josef94/opencv.git
-cd opencv/build
-chmod +x buildOpenCV
-./buildOpenCV
-make 
-sudo make install
-echo -e "$Yellow OpenCV successfully installed $OFF"
+############################# Ordnerstruktur #############################
+echo -e "$Cyan Ordnerstruktur erstellen... $Off"
+cd /BA
+if [ ! -d /BA/Crops ]
+then
+  mkdir /BA/Crops
+fi
 
-echo -e "$Cyan Make all $OFF"
-cd /BA/CheckMotion/Release
-cmake .
-make clean 
-make all 
+if [ ! -d /BA/Frames ]
+then
+  mkdir /BA/Frames
+fi
 
-cd /BA/VehicleCount/Release
-cmake .
-make clean 
-make all
-echo -e "$Yellow Make all finished $OFF"
+if [ ! -d /BA/Videos ]
+then
+  mkdir /BA/Videos
+fi
 
-echo -e "$Cyan chmod +x $OFF"
-cd /BA/
-chmod +x MakeVideo/makeVideo.sh
-chmod +x Handler/Release/handler.sh
-echo -e "$Cyan chmod +x $OFF"
+############################# OpenCV  #############################
+echo -e "$Cyan Installing OpenCV... $OFF"
+if ! dpkg --get-selections | grep -q build-essential
+then
+  echo -e "$Yellow build-essential $OFF"
+  sudo apt-get -y -q install build-essential
+fi
 
+if ! dpkg --get-selections | grep -q cmake
+then
+  echo -e "$Yellow cmake $OFF"
+  sudo apt-get -y -q install cmake
+fi
 
+if ! dpkg --get-selections | grep -q git
+then
+  echo -e "$Yellow git $OFF"
+  sudo apt-get -y -q install git
+fi
 
+if ! dpkg --get-selections | grep -q libgtk2.0-dev
+then
+  echo -e "$Yellow libgtk2.0-dev $OFF"
+  sudo apt-get -y -q install libgtk2.0-dev
+fi
 
+if ! dpkg --get-selections | grep -q pkg-config
+then
+  echo -e "$Yellow pkg-config $OFF"
+  sudo apt-get -y -q install pkg-config
+fi
 
+if ! dpkg --get-selections | grep -q libavcodec-dev
+then
+  echo -e "$Yellow libavcodec-dev $OFF"
+  sudo apt-get -y -q install libavcodec-dev
+fi
 
+if ! dpkg --get-selections | grep -q libavformat-dev
+then
+  echo -e "$Yellow libavformat-dev $OFF"
+  sudo apt-get -y -q install libavformat-dev
+fi
 
-echo -e "$Green Finished"
+if ! dpkg --get-selections | grep -q libswscale-dev
+then
+  echo -e "$Yellow libswscale-dev $OFF"
+  sudo apt-get -y -q install libswscale-dev
+fi
+
+if ! pkg-config --exists opencv
+then
+  echo -e "$Yellow OpenCV $OFF"
+  if [ ! -d ~/opencv ]
+  then
+  cd ~/
+  git clone https://github.com/josef94/opencv.git
+  fi
+
+  if [ -d ~/opencv/build ]
+  then
+    if [[ ! ~/opencv/build/CMakeFiles ]]
+    then
+    cd ~/opencv/build
+    chmod +x buildOpenCV
+    ./buildOpenCV
+    fi
+
+    if [[ -d ~/opencv/build/CMakeFiles && ! -f ~/opencv/build/Makefile ]]
+    then
+    cd ~/opencv/build
+    make
+    fi
+
+    if [ -f ~/opencv/build/Makefile ]
+    then
+    cd ~/opencv/build
+    sudo make install
+    echo -e "$Yellow OpenCV successfully installed $OFF"
+    fi
+  fi
+fi
+
+############################# avconv  #############################
+echo -e "$Cyan Installing Avconv... $OFF"
+if ! dpkg --get-selections | grep -q libav-tools
+then
+  echo -e "$Yellow avconv $OFF"
+  sudo apt-get -y -q install libav-tools
+fi
+
+############################# Make all  #############################
+echo -e "$Cyan Make all... $OFF"
+if [ -d /BA/CheckMotion/Release ]
+then
+  cd /BA/CheckMotion/Release
+  if [ ! -d /BA/CheckMotion/Release/CMakeFiles ]
+  then
+  cmake .
+  fi
+
+  if [ -d /BA/CheckMotion/Release/CMakeFiles ]
+  then
+  make clean
+  make all
+  fi
+fi
+
+if [ -d /BA/VehicleCount/Release ]
+then
+  cd /BA/VehicleCount/Release
+  if [ ! -d /BA/VehicleCount/Release/CMakeFiles ]
+  then
+  cmake .
+  fi
+
+  if [ -d /BA/VehicleCount/Release/CMakeFiles ]
+  then
+  make clean
+  make all
+  fi
+fi
+
+############################# chmod  #############################
+echo -e "$Cyan chmod... $OFF"
+if [ -f /BA/MakeVideo/makeVideo.sh ]
+  then
+  chmod +x /BA/MakeVideo/makeVideo.sh
+  fi
+
+if [ -f /BA/Handler/Release/handler.sh ]
+  then
+  chmod +x /BA/Handler/Release/handler.sh
+  fi
+
+echo -e "$Green Finished $OFF"

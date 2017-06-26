f702989c1892864f5b956f49e955efd827dd65ef
josef94 <josef.boeckle@gmail.com>
2017-06-26 12:58:23 +0200

    for Allways on

--- Handler/Release/settings.sh
+++ Handler/Release/settings.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ ! -d /tmp/videoStart ]
+ if [ ! -d /BA/Temp/videoStart ]
  then
+  then
    export LD_LIBRARY_PATH=/usr/local/lib
    mjpg_streamer -i "input_uvc.so -d /dev/video0" -o "output_http.so -p 8080 -w /usr/local/www" &
+ fi
 fi
